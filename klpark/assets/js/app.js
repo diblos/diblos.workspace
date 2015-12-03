15113feeb6dcd52c3f66e50b001efbf04b71ee69
Bakhtiar <diblos@gmail.com>
2015-12-04 06:37:01 +0800

    Change App to use AngularJS
    Change App using AngularJS

--- klpark/assets/js/app.js
+++ klpark/assets/js/app.js
@@ -1,61 +1,26 @@
-var theader = '<tr><th>&nbsp;</th><th>Lokasi</th><th style="text-align:center;">Jumlah Kosong</th></tr>';
-var tInterval = 60000;
+'use strict';
+var app = angular.module('myApp', []);
 
-$( document ).ready(function(){	Y();});
+app.controller('MallListCtrl', function($scope, $http, $timeout) {
+	// Function to get the data
+	$scope.getData = function(){
+		$http.get('data.php')
+			.success(function(response) {
+			$scope.malls = response.query.results.PGIS;
+			$scope.TIMESTAMP = clock();
+			console.log('Fetched data!');
+		});
+	};
+	// Function to replicate setInterval using $timeout service.
+	$scope.intervalFunction = function(){
+		$timeout(function() {
+			$scope.getData();
+			$scope.intervalFunction();
+		}, 15000)
+	};
+	$scope.getData();
+	$scope.intervalFunction();// Kick off the interval
+});
 
-var notify = function(dMsg){$("#alert_placeholder").html('<div class="alert alert-warning fade in"><a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a><strong>Warning!</strong> '+dMsg+'</div>');};
-var URL = 'data.php';
-
-var Y = function(){
-		
-			//beforeSend: function() { $.mobile.loading('show'); }, //Show spinner
-            //complete: function() { $.mobile.loading('hide'); }, //Hide spinner	
-			count =1;
-			$.ajax({
-			method: "GET",
-            url: URL,
-            dataType: "xml",
-            success: function(data) {
-			
-				//alert(data);
-				
-				//return false;
-				 
-				$('#table-list > tbody:last').empty();
-				$('#table-list > tbody:last').append(theader);
-				 
-				try{
-		
-					$(data).find('MALL').each(function(){
-					var xLokasi = $(this).find('NAME').text();
-					var xPetak = $(this).find('LOT').text();
-					var xTimestamp = $(this).find('DATETIME').text();
-					
-					if(xLokasi!==''){
-					$('#table-list > tbody:last').append('<tr><td><span class="label label-info">'+ count++ +'</span></td><td style="text-align:left;">'+xLokasi+'</td><td style="text-align:center;"><span class="badge">'+xPetak+'</span></td></tr><tr>');					
-					// var d = new Date(xTimestamp);
-					$('#timestamp').html("Timestamp: "+xTimestamp);
-					}					
-					
-					});
-
-				}catch(err){
-					alert(err.message);
-				}	
-
-						// $('#timestamp').html(Date());
-						setTimeout(function(){Y();},tInterval);
-				
-				
-            },
-           	error: function(x, t, m) {
-		        if(t==="timeout") {
-		            setTimeout(function(){notify("err:timeout");},1000);
-		        } else {
-		        	notify(x+' '+t+' '+m);
-		        }
-    		}
-        });
-		
-
-}
+function clock() {var now = new Date();var TmStr = 'Waktu: ' + (now.getDate())+"/"+(now.getMonth()+1)+"/"+now.getFullYear() + ' ' + ('0' + now.getHours()).slice(-2) + ':' + ('0' + now.getMinutes()).slice(-2) + meridiem(now.getHours());return TmStr;}
+function meridiem(hour) { if (hour > 11) { return 'PM'; } else { return 'AM'; }}
--- klpark/data.php
+++ klpark/data.php
@@ -1,9 +1,36 @@
 <?php
-$url = 'http://dbklpgis.scadatron.net/dbklpgisd1.xml';
-// $xml = new SimpleXMLElement($url, null, true);
-// echo $xml->results->quote[0]->AskRealtime;
-// print_r($xml);
+// header("Content-type: aplication/json; charset=utf-8");
+header("Content-type: text/plain; charset=utf-8");
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST");
 
-$contents = file_get_contents($url);
-print_r($contents);
+$url='http://dbklpgis.scadatron.net/dbklpgisd1.xml';
+//Code to access YQL using PHP
+// $yql_query = "select * from xml where url='".$url."' and itemPath=\"//PGIS/MALL\"";
+$yql_query = "select MALL from xml where url='".$url."' and MALL.ID<>'M0'";
+ 
+$result = getResultFromYQL(sprintf($yql_query),'store%3A%2F%2Fdatatables.org%2Falltableswithkeys');
+
+echo $result;
+
+function getResultFromYQL($yql_query, $env = '') {
+    $yql_base_url = "http://query.yahooapis.com/v1/public/yql";
+    $yql_query_url = $yql_base_url . "?q=" . urlencode($yql_query);
+    $yql_query_url .= "&format=json";
+
+    if ($env != '') {
+        $yql_query_url .= '&env=' . urlencode($env);
+    }
+
+    $session = curl_init($yql_query_url);
+    curl_setopt($session, CURLOPT_RETURNTRANSFER, true);
+    //Uncomment if you are behind a proxy
+    //curl_setopt($session, CURLOPT_PROXY, 'Your proxy url');
+    //curl_setopt($session, CURLOPT_PROXYPORT, 'Your proxy port');
+    //curl_setopt($session, CURLOPT_PROXYUSERPWD, 'Your proxy password');
+    $json = curl_exec($session);
+    curl_close($session);
+    // return json_decode($json);
+    return $json;
+}
 ?>
