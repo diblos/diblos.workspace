5ef994aee092990a5f47c002a85a5d30d2738105
diblos <diblos@gmail.com>
2017-04-09 23:51:12 +0800

    modification: data read size

--- phpSocket/server2client/client.php
+++ phpSocket/server2client/client.php
@@ -3,10 +3,9 @@
 define("KB", 1024, true);
 define("MB", 1048576, true);
 define("DESTINATION_PATH","C:/Users/lenovo/Documents/GitHub/workspace/phpSocket/server2client/",true);
-$address = "127.0.0.1";
-// $address = "rdp.seamcloud.com";
-$port = 10000;
-// $port = 33223;
+// $address = "127.0.0.1";$port = 10000;
+$address = "rdp.seamcloud.com";$port = 33223;
+
 $nsize = 500*KB;
 
 //require('..\lib.php');//HERE WIN
@@ -26,9 +25,10 @@ if ($result === false) {
 } else {
     echo "successfully connected to $address.\n";
 }
+$command_str = "OTA:2{s:3:DEV;s:16:0001010053415931;}";
+// $command_str = "OTA:2{s:3:DEV;s:3:123;}";
 
-// socket_write($socket,"GET");// <-- First Command for requesting file.
-socket_write($socket,"OTA:2{s:3:DEV;s:16:0001010053415931;}");// <-- First Command for requesting file.
+socket_write($socket,$command_str);// <-- First Command for requesting file.
 
 $file = ''; // <-- this is 2 apostrophies by the way
 
@@ -42,27 +42,45 @@ if ((isset($tmp))&&(count($tmp)==3)){
     echo "CRC : $checksum, Filename : $filename, Filesize : $filesize".PHP_EOL;
     socket_write($socket,ACK);
 
+    $dataleft = $filesize;
+    $chunksize = CHUNK_SIZE;
     // FILE TRANSMISSION PROCESS START
-        $file = fopen(DESTINATION_PATH.$filename,"w");
-    // while (false !== ($bytes = socket_recv($socket, $buf, CHUNK_SIZE, MSG_WAITALL))) {
-    while (false !== ($bytes = socket_recv($socket, $buf, 4000, MSG_WAITALL))) {
+    $file = fopen(DESTINATION_PATH.$filename,"w");
+
+    while (false !== ($bytes = socket_recv($socket, $buf, $chunksize, MSG_WAITALL))) {
         echo "Read $bytes bytes from socket_recv()...".PHP_EOL;
         echo (socket_strerror(socket_last_error($socket)).PHP_EOL);
         // echo strlen($buf).PHP_EOL;
-        fwrite($file,$buf,$bytes);
+        $fwrite = fwrite($file,$buf,$bytes);
+        $dataleft = $dataleft - $fwrite;
+
+        echo("Data left: $dataleft".PHP_EOL);
+
+        //SANITY CHECKING
+
+
         // sleep(1);
         socket_write($socket,ACK);
+        if ($dataleft<$chunksize) $chunksize = $dataleft;
     }
     fclose($file);
     // FILE TRANSMISSION PROCESS END
+    // =============================================================================
+    // $lchecksum = CRCfile(DESTINATION_PATH.$filename);// CRC 16
+    // -----------------------------------------------------------------------------
+    $lchecksum = md5_file(DESTINATION_PATH.$filename);// MD5 HASH
+    // =============================================================================
 
-    if( CRCfile(DESTINATION_PATH.$filename) == $checksum ){
+    if( $lchecksum == $checksum ){
       echo "checksum success.".PHP_EOL;
+      // echo($lchecksum).PHP_EOL);
+      // echo(crc16("0123456789").PHP_EOL);
     }else{
       echo "checksum failed.".PHP_EOL;
       unlink(DESTINATION_PATH.$filename);
     }
 }else {
+    echo "send NACK!".PHP_EOL;
     socket_write($socket,NACK);
 }
 
--- phpSocket/server2client/server.php
+++ phpSocket/server2client/server.php
@@ -1,5 +1,6 @@
 <?php
 /* SERVER2CLIENT : SERVER */
+define('SOCKET_READ_TIMEOUT',60);
 define("KB", 1024, true);
 define("MB", 1048576, true);
 define("SOURCE_PATH","C:/Users/lenovo/Desktop/workspace/4me/@work/forDeploy/files/20170113/final/",true);
@@ -12,6 +13,8 @@ $nsize = 500*KB;
 require('../lib.php');//HERE LINUX
 
 $mysock = socket_create(AF_INET,SOCK_STREAM,SOL_TCP);
+// if(!socket_set_option($sock, SOL_SOCKET, SO_REUSEADDR, 1)){echo 'Unable to set SO_REUSEADDR option on socket: '.socket_strerror(socket_last_error()).PHP_EOL;};
+// if(!socket_set_option($sock,SOL_SOCKET, SO_RCVTIMEO, array("sec"=>SOCKET_READ_TIMEOUT, "usec"=>0))){echo 'Unable to set SO_RCVTIMEO option on socket: '.socket_strerror(socket_last_error()).PHP_EOL;};
 
 socket_bind($mysock,$address,$port);
 
@@ -29,16 +32,18 @@ if($input!==false && !empty($input)){
 
   echo "<< $input".PHP_EOL;
 
-  $checksum = CRCfile(SOURCE_PATH.FILENAME);
+  // =============================================================================
+  // $checksum = CRCfile(SOURCE_PATH.FILENAME);// CRC 16
+  // -----------------------------------------------------------------------------
+  $checksum = md5_file(SOURCE_PATH.FILENAME);// MD5 HASH
+  // =============================================================================
   $response = $checksum."|".FILENAME."|".filesize(SOURCE_PATH.FILENAME);
-
   // DISPLAY OUTPUT  BACK TO CLIENT
   echo ">> $response".PHP_EOL;
   socket_write($client, $response);
 
   // READ RESPONSE
   $input = socket_read($client,$nsize);
-
   var_dump($input);
 
   if(($input!==false || !empty($input))&&($input==ACK)){
@@ -49,25 +54,42 @@ if($input!==false && !empty($input)){
           $fp = fopen(SOURCE_PATH.FILENAME,"r") ;
           if ($fp!==false){
   							$total = 0;
+                $TCP_ERR = false;
                 while (! feof($fp)) {
 					       		$buff = fread($fp,CHUNK_SIZE);
 					       		echo "sending a block of ".FILENAME." [" . strlen($buff) ."]".PHP_EOL;
 					       		$total+=strlen($buff);
 
-                    $csm = crc16($buff);
-                    $buffc = pack($csm,$buff);
+                    // $csm = crc16($buff);
+                    // $buffc = pack($csm,$buff);
 
-                    echo strlen($buff).">>".strlen($buffc).PHP_EOL;
+                    // echo strlen($buff).">>".strlen($buffc).PHP_EOL;
 
 					       		socket_write($client, $buff);
 
                     // READ RESPONSE
                     $input = socket_read($client,$nsize);
+                    switch ($input) {
+                        case ACK:
+                            // RECEIVE ACK & PROCEED TO NEXT PACKET
+                            echo("Acknowledged!".PHP_EOL);
+                            $TCP_ERR = false;
+                            break;
+                        case NACK:
+                            // RECEIVE NACK & RESEND CURRENT PACKET
+                            echo("Not Acknowledged!".PHP_EOL);
+                            $TCP_ERR = false;
+                            break;
+                        default:
+                            // ERROR: EXIT LOOP & OTA PROCESS
+                            echo("Invalid Response!".PHP_EOL);
+                            $TCP_ERR = true;
+                    }
 
 				       	}
 				       	echo "have sent $total bytes".PHP_EOL;
 				       	fclose($fp);
-                // socket_write($client, null);
+                socket_write($client, null);
 					}
 
       }
@@ -78,9 +100,7 @@ if($input!==false && !empty($input)){
 }else{
   echo "wrong input: ".serialize($input).PHP_EOL;
 }
-
-
-
+sleep(10);
 socket_close($client);
 socket_close($mysock);
 
